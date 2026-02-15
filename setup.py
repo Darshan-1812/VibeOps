748b24ac7a0556ad3b3b1b6d5e370da58b221ea4
darshangirase <Darshan-1812@users.noreply.github.com>
2026-02-15 16:09:39 +0530

    Update MLflow URI and pipeline configs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlops-project',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='darshan girase',
+    author_email= 'darshangirase18@gmail.com'
+    description='End-to-end MLOps Project',
+)
